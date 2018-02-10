975b9c931b5459706483abf618c4d3241de56814
John <john.milner@mac.com>
2018-02-10 15:48:24 -0800

    add profile and image endpoints

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 
 const app = express();
@@ -23,11 +24,18 @@ const database = {
             entries: 0,
             joined: new Date()
         }
+    ], 
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
     ]
 }
 
 app.get('/', (req, res) => {
-    res.send('this is working!!!');
+    res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
@@ -37,6 +45,53 @@ app.post('/signin', (req, res) => {
         } else {
             res.status(400).json('error logging in');
         }
+});
+
+app.post('/register', (req, res) => {
+    const { email, name, password} = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash)    
+    });
+    database.users.push(
+        {
+            id: '125',
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+        }
+    );
+    res.json(database.users[database.users.length - 1])
+});
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        } 
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+});
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        } 
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
 });
 
 app.listen(3000, () => {
