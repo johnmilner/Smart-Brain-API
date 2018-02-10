96a9cb01e1f3973bdf07c26cd179f09003288127
John <john.milner@mac.com>
2018-02-09 23:06:00 -0800

    add signin functionality

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'john',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) => {
-    res.send('this is working!!!')
-})
+    res.send('this is working!!!');
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('success');
+        } else {
+            res.status(400).json('error logging in');
+        }
+});
 
 app.listen(3000, () => {
     console.log('app is running on port 3000!');
