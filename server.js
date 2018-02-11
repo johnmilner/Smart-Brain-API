4ff153ceab2655bf96eb16fa06862eae62aa58f6
John <john.milner@mac.com>
2018-02-10 22:19:34 -0800

    change localhost address to 3001

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
+
 
 const database = {
     users: [
@@ -41,7 +45,8 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-        res.json('success');
+        //res.json('success');
+        res.json(database.users[0]);
         } else {
             res.status(400).json('error logging in');
         }
@@ -94,8 +99,8 @@ app.put('/image', (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000!');
+app.listen(3001, () => {
+    console.log('app is running on port 3001!');
 });
 
 /*
