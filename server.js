a20419a3d2f629db943eea8ebd187906420c58ce
John <john.milner@mac.com>
2018-02-12 14:57:02 -0800

    complete register endpoint with knex transaction

--- server.js
+++ server.js
@@ -59,28 +59,33 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-        //res.json('success');
-        res.json(database.users[0]);
-        } else {
-            res.status(400).json('error logging in');
-        }
+    
 });
 
 app.post('/register', (req, res) => {
     const { email, name, password} = req.body;
-    // bcrypt.hash(password, null, null, function(err, hash) {
-    //     console.log(hash)    
-    db('users')
-    .returning('*')
-    .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    }).then(user => {
-        res.json(user[0]);
-    })
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date()
+            }).then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })    
     .catch(err => res.status(404).json('unable to register'));
 });
 
