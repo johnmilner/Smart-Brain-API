2ec904080d7e5034aa30a9c555653c9188b35f12
John Milner <john.milner@mac.com>
2018-02-16 15:33:47 -0800

    refactor endpoints in controllers file

---
+++ controllers/profile.js
@@ -0,0 +1,17 @@
+const handleProfileGet = (req, res, db) => {
+    const { id } = req.params;
+    let found = false;
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if (user.length) {
+            res.json(user[0]);
+        } else {
+            res.status(400).json('Not Found')
+        }
+    })
+    .catch(err => res.status(400).json('Error getting user'))
+}
+
+module.exports = {
+    handleProfileGet: handleProfileGet
+}
