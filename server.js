e146ff1b18015258d6bb807803aef77d4f6c1d7c
John Milner <john.milner@mac.com>
2018-02-17 00:28:28 -0800

    update console log

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('app is running on port ${process.env.PORT}');
+    console.log(`app is running on port ${process.env.PORT}`);
 });
 
 /*
