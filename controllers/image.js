034690ff9085f7fc7688a3243f2e993568342fca
John Milner <john.milner@mac.com>
2018-02-16 17:28:51 -0800

    refactor for security review in section 199

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'fedae956b51f4f0c96143a119a66f7fe'
+  });
+
+const handleApiCall = (req, res) => {
+    app.models
+   .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+   .then(data => {
+       res.json(data);
+   })
+   .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
