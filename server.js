15db36d2211323d897ed84318e5d0d1940f288c4
John Milner <john.milner@mac.com>
2018-02-17 00:17:11 -0800

    update database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-graceful-99751',
       user : 'johnnycatsd',
       password : 'dbdb',
       database : 'smart_brain'
